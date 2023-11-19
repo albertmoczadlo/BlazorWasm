b8294ce01ee30085558f819ff096ac2df2b78d56
Albert <albertmoczadlo@gmail.com>
2023-11-19 20:45:33 +0100

    Add project files.

---
+++ Pages/Home.razor.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWasm.Pages
+{
+    public partial class Home
+    {
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+        private void BackHomework()
+        {
+            NavigationManager.NavigateTo("/homework-parametr/parametr");
+        }
+    }
+}
---
+++ Pages/Homework.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Reflection.Metadata;
+
+namespace BlazorWasm.Pages
+{
+    public partial class Homework
+    {
+        private string _header;
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Parameter]
+        public string Header
+        {
+            get =>_header;
+
+            set => _header= string.IsNullOrWhiteSpace(value)?"Praca domowa":value;
+        }
+
+        private void BackHome()
+        {
+            NavigationManager.NavigateTo("/");
+        }
+    }
+}
