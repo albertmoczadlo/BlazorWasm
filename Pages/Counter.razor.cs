5393980cfd260905b4bfd6bd2fb6af7b733dfba1
Albert <albertmoczadlo@gmail.com>
2023-11-23 08:58:30 +0100

    Added exception handling

---
+++ Pages/Counter.razor.cs
@@ -0,0 +1,21 @@
+﻿namespace BlazorWasm.Pages
+{
+    public partial class Counter
+    {
+        public int currentCount = 0;
+
+        public void IncrementCount()
+        {
+            try
+            {
+                throw new Exception("This is a test exception.");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            currentCount++;
+        }
+    }
+}
